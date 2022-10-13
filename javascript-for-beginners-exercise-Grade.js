a19dcbf435d416a1b09c6978024668bbeef2ff5f
Ade-mir <115159286+Ade-mir@users.noreply.github.com>
2022-10-13 14:09:19 +0200

    FInished module 4 in JavaScript for beginners

--- javascript-for-beginners-exercise-Grade.js
+++ javascript-for-beginners-exercise-Grade.js
@@ -1,26 +1,48 @@
 // Avarage = 70
 
-// 1-59: F
+// 0-59: F
 // 60-69: D
 // 70-79: C
 // 80-89: B
 // 90-100: A
 
-const marks = [90, 40];
+// How I did it.
+// const marks = [90, 40];
+
+// console.log(calculateGrade(marks));
+
+// function calculateGrade(marks) {
+//   const initialValue = 0;
+//   const sumWithInitial = marks.reduce(
+//     (previousValue, currentValue) => previousValue + currentValue,
+//     initialValue
+//   );
+//   const avarageOfMarks = sumWithInitial / Object.keys(marks).length;
+
+//   if (avarageOfMarks <= 59) return 'F';
+//   if (avarageOfMarks >= 60 && avarageOfMarks < 70) return 'D';
+//   if (avarageOfMarks >= 70 && avarageOfMarks < 80) return 'C';
+//   if (avarageOfMarks >= 80 && avarageOfMarks < 90) return 'B';
+//   if (avarageOfMarks >= 90 && avarageOfMarks < 100) return 'A';
+// }
+
+// How Mosh did it.
+
+const marks = [90, 50, 90];
 
 console.log(calculateGrade(marks));
 
 function calculateGrade(marks) {
-  const initialValue = 0;
-  const sumWithInitial = marks.reduce(
-    (previousValue, currentValue) => previousValue + currentValue,
-    initialValue
-  );
-  const avarageOfMarks = sumWithInitial / Object.keys(marks).length;
+  const average = calculateAverage(marks);
+  if (average < 60) return 'F';
+  if (average < 70) return 'D';
+  if (average < 80) return 'C';
+  if (average < 90) return 'B';
+  return 'A';
+}
 
-  if (avarageOfMarks <= 59) return 'F';
-  if (avarageOfMarks >= 60 && avarageOfMarks < 70) return 'D';
-  if (avarageOfMarks >= 70 && avarageOfMarks < 80) return 'C';
-  if (avarageOfMarks >= 80 && avarageOfMarks < 90) return 'B';
-  if (avarageOfMarks >= 90 && avarageOfMarks < 100) return 'A';
+function calculateAverage(array) {
+  let sum = 0;
+  for (let value of array) sum += value;
+  return sum / array.length;
 }
