c2b7eb8bea79ec4d96d858af46c58aff5f19c805
Ade-mir <115159286+Ade-mir@users.noreply.github.com>
2022-10-17 17:03:10 +0200

    Renamed all files

---
+++ Javascript-Basics-for-Beginners-Section-5-1-Basics.js
@@ -0,0 +1,15 @@
+// Object Oriented Programming (OOP)
+
+const circle = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1,
+  },
+  isVisible: true,
+  draw: function () {
+    console.log('draw');
+  },
+};
+
+circle.draw(); // Calling a function which is saved as a "Key value pair" inside of an object means you are calling a Method.
