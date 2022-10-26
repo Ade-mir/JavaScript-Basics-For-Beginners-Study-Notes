93ee0ce5f965c460db0ba5617d968f1bb61f80ed
Ade-mir <115159286+Ade-mir@users.noreply.github.com>
2022-10-26 14:42:32 +0200

    Studied

--- Javascript-Basics-for-Beginners-Section-6-16-Reducing-an-Array.js
+++ Javascript-Basics-for-Beginners-Section-6-16-Reducing-an-Array.js
@@ -1,6 +1,10 @@
 const numbers = [1, -1, 2, 3];
 
-let sum = 0;
-for (let n of numbers) sum += n;
+// let sum = 0;
+// for (let n of numbers) sum += n;
+
+const sum = numbers.reduce(
+  (accumulator, currentValue) => accumulator + currentValue
+);
 
 console.log(sum);
